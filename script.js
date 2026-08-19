920e12297ae00c389b77e1ca60368a61b7f181e0
Nilton Cleverson <softnil.suporte@gmail.com>
2026-08-19 11:01:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -36,10 +36,10 @@ async function detectAPI() {
             }
           if (apelido != '' || txt != ''){
             // Faz o POST para o servidor Lazarus na porta 8989
-            fetch("https://nonnihilistic-lita-unpanniered.ngrok-free.dev/enviar", {
+            fetch("http://softnil.ddnsgratis.com.br:8989/enviar", {
    method: "POST",
 headers: { "Content-Type": "application/x-www-form-urlencoded" },
-body: "mensagem="+encodeURIComponent(apelido)+"!"+encodeURIComponent(apelido)+"@"+encodeURIComponent(apelido)+".tmi.twitch.tv PRIVMSG #"+encodeURIComponent(apelido)+ ':' + encodeURIComponent(txt)
+body: "?mensagem="+encodeURIComponent(apelido)+"!"+encodeURIComponent(apelido)+"@"+encodeURIComponent(apelido)+".tmi.twitch.tv PRIVMSG #"+encodeURIComponent(apelido)+ ':' + encodeURIComponent(txt)
 })
                showToast("Musica adicionada a lista");
               document.getElementById("texto").value="";
