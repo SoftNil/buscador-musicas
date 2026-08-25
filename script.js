3fd6745379234d4d9e7c834bee2f447b92aeb3ac
Nilton Cleverson de Oliveira <59276374+SoftNil@users.noreply.github.com>
2026-08-24 22:39:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -134,10 +134,10 @@ function displayResults(data){
       const li = document.createElement('li');
       li.className = 'list-group-item d-flex justify-content-between align-items-center';
 
-    const buttonsHTML = '<button class="btn btn-sm btn-success btn-pedir"><i class="fas fa-music"></i> Pedir</button><button class="btn btn-sm btn-danger btn-youtube"><i class="fab fa-youtube"></i> YouTube</button>';
+    const buttonsHTML = '<button class="btn btn-sm btn-success btn-pedir"><i class="fas fa-music"></i> Pedir</button>';
 
-      const youtubeBtn = `<button class="btn btn-sm btn-danger"><i class="fab fa-youtube"></i> YouTube</button>`;
-      li.innerHTML = `<span>${song.title}</span><span class="song-buttons">${buttonsHTML} ${youtubeBtn}</span>`;
+      const youtubeBtn = '<button class="btn btn-sm btn-danger btn-youtube"><i class="fab fa-youtube"></i> YouTube</button>';
+    
 
      const btnPedir = li.querySelector('.btn-pedir');
 const btnYoutube = li.querySelector('.btn-youtube');
